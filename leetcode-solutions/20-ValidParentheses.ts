375af323f990a7bebffc9c21299ff563de6c9ad5
Cloviski <claudioribeiro.j@hotmail.com>
2024-01-29 16:18:11 -0300

    Added 20th leetcode

---
+++ leetcode-solutions/20-ValidParentheses.ts
@@ -0,0 +1,55 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+*/
+
+function isValid(s: string): boolean {
+  const stack: string[] = [];
+
+  for (let i = 0; i < s.length; i++) {
+    const char: string = s[i];
+
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+    } else {
+      const top: string = stack.pop()!;
+
+      if (
+        (char === ")" && top !== "(") ||
+        (char === "}" && top !== "{") ||
+        (char === "]" && top !== "[")
+      ) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(isValid("(]"));
